ff7a224909645d132d17400164948da3e7228fb6
hkichen <hkichen@gmail.com>
2018-06-20 16:40:01 -0400

    fixed json file

--- app/src/App.js
+++ app/src/App.js
@@ -1,12 +1,25 @@
 import React, { Component } from "react";
 import MemoryCard from "./components/MemoryCard";
 import Wrapper from "./components/Wrapper";
-import Nav from "./components/Nav";
+import Header from "./components/Header";
 import princesses from "./princesses.json";
 import "./App.css";
 
 class App extends Component {
-  render() 
+  render() {
+    return (
+      <Wrapper>
+        <Header>Princess Memory Game</Header>
+        <MemoryCard
+          key={princess.id}
+          image={princess.image}
+          clicked={princess.clicked}
+          onClicked={this.changeStatus}
+        />
+      </Wrapper>
+    )
+  }
+    
 
 }
 
